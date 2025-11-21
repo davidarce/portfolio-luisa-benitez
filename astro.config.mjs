b9e04220f353d37a45d3d55ed84f9da1d0529d5f
David Arce <davidaser@ext.inditex.com>
2025-11-21 12:42:49 +0100

    feat: Add deploy config for Github Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://davidarce.github.io',
+  base: '/portfolio-luisa-benitez',
   devToolbar: {
     enabled: false
   },
